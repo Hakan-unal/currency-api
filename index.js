87a8e40a8ae5b880544bde3f8f87ec1804c204c9
Con Sınov <hakan.unal@radity.com>
2023-12-18 02:59:55 +0300

    updated

--- index.js
+++ index.js
@@ -2,20 +2,36 @@ const express = require("express")
 const morgan = require("morgan")
 const bodyParser = require("body-parser")
 const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config();
 
 const app = express();
 
 // using morgan for logs
 app.use(morgan('combined'));
 
+const api_URL= `https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.S.YTL&startDate=02-01-2005&endDate=01-02-2017&type=xml&key=${process.env.TCMB_API_KEY}&aggregationTypes=avg&formulas=1&frequency=8`
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => {
+app.get('/', async(req, res) => {
     res.send("Hello World");
+
+});
+
+
+app.get('/tcmb', async(req, res) => {
+
+     fetch(api_URL).then((response)=>{
+        res.send(response)
+     }).catch(
+        res.send("err")
+
+     )
+
 });
 
 
