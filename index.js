0c4e9ee0dd1ff276d728e19003bd035c2452cda7
Con Sınov <hakan.unal@radity.com>
2023-12-18 03:17:26 +0300

    updated

--- index.js
+++ index.js
@@ -13,14 +13,14 @@ app.use(morgan('combined'));
 const api_URL= `https://evds2.tcmb.gov.tr/service/evds/series=TP.DK.USD.S.YTL&startDate=02-01-2005&endDate=01-02-2017&type=xml&key=${process.env.TCMB_API_KEY}&aggregationTypes=avg&formulas=1&frequency=8`
 
 app.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
-app.use(bodyParser.json());
 app.use(cors());
 
 
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
 const options = { customCssUrl: '/public/swagger-ui.css', customSiteTitle: "The Words That I Know API - Swagger" };
-
+app.use(express.json());
+app.use(express.static("/api-docs"));
 
 app.use('/api-docs', swaggerUi.serve);
 app.get('/api-docs', swaggerUi.setup(swaggerDocument,options));
